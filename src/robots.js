8e8d244a746249b5de4583fe53dcb67487403952
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-07-26 14:17:08 +0300

    initial setup, creating the cards

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+    {
+        id:1,
+        name:"Veneta",
+        username:"Vencheto0",
+        email: 'veneta@gmail.com'
+    },
+    {
+        id:2,
+        name:"Toni",
+        username:"Tonika",
+        email: 'toni@gmail.com'
+    },
+    {
+        id:3,
+        name:"Lora",
+        username:"Loreana",
+        email: 'lora@gmail.com'
+    },
+    {
+        id:4,
+        name:"Desimir",
+        username:"Deso",
+        email: 'desimir@gmail.com'
+    },
+    {
+        id:5,
+        name:"Iva",
+        username:"ivcheto",
+        email: 'iva@gmail.com'
+    },
+    {
+        id:6,
+        name:"Miroslava",
+        username:"Mirka",
+        email: 'miroslava@gmail.com'
+    },
+    {
+        id:7,
+        name:"Teo",
+        username:"chipikao",
+        email: 'teodosi@gmail.com'
+    },
+    {
+        id:8,
+        name:"Ralica",
+        username:"Ralichka",
+        email: 'ralica@gmail.com'
+    },
+    {
+        id:9,
+        name:"Polina",
+        username:"Poli",
+        email: 'polina@gmail.com'
+    }
+]
