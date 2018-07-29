6f24af50efb6b9d73ff49b954167f88e6fd2f7ac
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-07-29 19:43:19 +0300

    Adding Redux middleware

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 import App from './containers/App';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {searchRobots} from './reducers';
+import {createLogger} from 'redux-logger';
 
-const store = createStore(searchRobots);
+const logger = createLogger(); //middleware that listens for actions
+const store = createStore(searchRobots,applyMiddleware(logger));
 
 ReactDOM.render(
     //Provider takes care of passing down the store to all children down the tree of components
