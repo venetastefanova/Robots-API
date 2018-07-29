43bb274a57c1c73705c9a8f830092123a2bfe556
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-07-29 16:45:20 +0300

    adding Redux actions and reducers

---
+++ src/reducers.js
@@ -0,0 +1,17 @@
+import { CHANGE_SEARCH_FIELD } from '/constants.js';
+
+const initialState = {
+    searchField:''
+}
+
+//takes initialState parameters; every action is just an object
+export const searchRobots = (state=initialState, action={}) =>{
+    switch(action.type){
+        case CHANGE_SEARCH_FIELD:
+            // we receive an action CHANGE_SEARCH_FIELD, then return new object 
+            //that changes the state of searchField
+             return Object.assign({}, state, {searchField:action.payload});
+        default:
+        return state;
+    }
+}
