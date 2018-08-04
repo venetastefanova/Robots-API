4c9ce82844f3a4901e12de6888dfff56d4ef7031
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-08-04 08:47:24 +0300

    using lifecycle functions to stop rerendering unnecessary header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
-
+import CounterButton from './CounterButton';
 
 class Header extends Component{
     shouldComponentUpdate(nextProps, nextState){
         return false; // doesn't return the header component and doesn't rerender it 
     }
     render(){
-        console.log("header");
-       return  <h1>RoboFriends</h1>        
+        console.log("buttonCounter");
+       return  (
+           <div>
+               <CounterButton color={'red'}/>
+           <h1>RoboFriends</h1>   
+           </div>
+       );     
     }
 }
 
